7761b9b68cfbe095128cf751266964858cd15d3d
Alena Busova <96522313+al-busova@users.noreply.github.com>
2022-08-17 22:24:11 +0300

    finish hm

--- src/index.js
+++ src/index.js
@@ -1,68 +1,114 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import PisabayApiServise from './pixabay-servise';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const refs = {
-    formEl: document.querySelector('#search-form'),
-    boxForGallery: document.querySelector('.gallery'),
-loadMoreBtn: document.querySelector('.load-more'),
-}
+  formEl: document.querySelector('#search-form'),
+  boxForGallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
 
 const pisabayApiServise = new PisabayApiServise();
+const lightBoxGallery = new SimpleLightbox('.gallery a', {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
 
 refs.formEl.addEventListener('submit', searchPhoto);
 refs.loadMoreBtn.addEventListener('click', loadMorePhoto);
 
 function searchPhoto(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    clearContent();
-    pisabayApiServise.query = e.currentTarget.elements.searchQuery.value.trim();
-    if (pisabayApiServise.query === '') {
-        Notiflix.Notify.info(
-          'Please enter something.'
+  clearContent();
+
+  pisabayApiServise.query = e.currentTarget.elements.searchQuery.value.trim();
+  if (pisabayApiServise.query === '') {
+    Notiflix.Notify.info('Please enter something.');
+    return;
+  }
+
+  pisabayApiServise.resetPage();
+  refs.loadMoreBtn.classList.add('is-hidden');
+  //      логіка через звичайні проміси
+  // pisabayApiServise.fetchPhoto()
+  //       .then((photos) => {
+  //         if (photos.totalHits === 0) {
+  //               Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+  //               return;
+  //         }
+  //         Notiflix.Notify.info(`Hooray! We found ${photos.totalHits} images.`);
+  //         refs.loadMoreBtn.classList.remove("is-hidden");
+  //         insertContent(photos.hits);
+  //       }
+  //   );
+  const insertCollection = async () => {
+    try {
+      const photos = await pisabayApiServise.fetchPhoto();
+      if (photos.totalHits === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
         );
         return;
+      }
+      Notiflix.Notify.success(`Hooray! We found ${photos.totalHits} images.`);
+      refs.loadMoreBtn.classList.remove('is-hidden');
+      insertContent(photos.hits);
+    } catch (error) {
+      Notiflix.Notify.failure(error.message);
+
+      console.log(error);
     }
-    pisabayApiServise.resetPage();
-    pisabayApiServise.fetchPhoto()
-        .then((photos) => {
-             if (photos.total === 0) {
-                Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-                return;
-            }
-                insertContent(photos.hits); 
-        } 
-        );
+  };
+  insertCollection();
+ 
 }
 
 function loadMorePhoto() {
-    pisabayApiServise.fetchPhoto().then((photos) => insertContent(photos.hits));
+  pisabayApiServise.fetchPhoto().then(photos => insertContent(photos.hits));
 }
 function insertContent(photos) {
-    refs.boxForGallery.insertAdjacentHTML('beforeend', createCardWithPhoto(photos));
-  
+  refs.boxForGallery.insertAdjacentHTML(
+    'beforeend',
+    createCardWithPhoto(photos)
+  );
+ lightBoxGallery.refresh();
+  const elemCount =
+    document.getElementsByClassName('gallery')[0].childElementCount;
+  if (elemCount === pisabayApiServise.totalHits) {
+    refs.loadMoreBtn.classList.add('is-hidden');
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
 }
+
 function createCardWithPhoto(photos) {
   return photos
     .map(
-      photo =>   `<div class="photo-card">
-  <img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" width= "300px"/>
+      photo => `<div class="photo-card">
+      <a class="gallery__item" href="${photo.largeImageURL}">
+  <img class="gallery__image" src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy"/>
+</a>
   <div class="info">
     <p class="info-item">
-      <b>Likes: ${photo.likes}</b>
+      <b>Likes <span>${photo.likes}</span></b>
     </p>
     <p class="info-item">
-      <b>Views: ${photo.views}</b>
+      <b>Views <span>${photo.views}</span></b>
     </p>
     <p class="info-item">
-      <b>Comments: ${photo.comments}</b>
+      <b>Comments <span>${photo.comments}</span></b>
     </p>
     <p class="info-item">
-      <b>Downloads: ${photo.downloads}</b>
+      <b>Downloads <span>${photo.downloads}</span></b>
     </p>
   </div>
-</div> `)
+</div> `
+    )
     .join('');
 }
 function clearContent() {
--- src/pixabay-servise.js
+++ src/pixabay-servise.js
@@ -1,22 +1,33 @@
+import axios from 'axios';
 export default class PisabayApiServise{
     constructor() {
         this.searchQuery = '';
         this.page = 1;
+        this.totalHits = 0;
     }
 
-    fetchPhoto() {
+    async fetchPhoto() {
         const API_KEY = '29314953-9960e0c1117cd8f48e1da89de';
         const url = `https://pixabay.com/api/?key=${API_KEY}&page=${this.page}&per_page=40&q=${this.searchQuery}&orientation=horizontal&image_type=photo&safesearch=true`;
-       
-        return fetch(url).then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        }).then(data => {
-            this.page += 1;
-            return data;
-        } );
+        
+        const response = await axios.get(url);
+        // const data = await response.json(); //після axios це прибирається
+        this.page += 1;
+        this.totalHits = response.totalHits;
+        return response.data;
+        // логіка через звичайні проміси
+        // return fetch(url)
+        //     .then(response => {
+        //     if (!response.ok) {
+        //         throw new Error(response.status);
+        //     }
+        //     return response.json();
+        //     })
+        //     .then(data => {
+        //     this.page += 1;
+        //     this.totalHits = data.totalHits;
+        //     return data;
+        // } );
     }
     resetPage() {
         this.page = 1;
@@ -27,4 +38,5 @@ export default class PisabayApiServise{
     set query(newQuery) {
        this.searchQuery = newQuery;
     }
+
 }
